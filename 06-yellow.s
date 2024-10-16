839af8197d51a33ec50c986fd6c46a36dc593710
Guilhermo Masid <guilhermomasid@gmail.com>
2024-10-16 19:45:26 -0300

    see where im going with this?

--- 06-yellow.s
+++ 06-yellow.s
@@ -4,6 +4,8 @@ section .data
 	addr db "yellow", 0x0a
 section .text
 _start:
+	mov [addr], byte 'H'
+	mov [addr + 5], byte '!' 
 	mov eax, 4 ; sys_write system call
 	mov ebx, 1 ; stdout file descriptor
 	mov ecx, addr ; bytes to write
