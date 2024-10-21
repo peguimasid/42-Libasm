e158fd1647c53774b4dc95be613847c853eb876c
Guilhermo Masid <guilhermomasid@gmail.com>
2024-10-21 20:01:40 -0300

    transform loop program into 64 bit

--- 04-loop.s
+++ 04-loop.s
@@ -2,15 +2,14 @@ global _start
 
 section .text
 _start:
-	mov ebx, 1 ; start ebx at 1
-  mov ecx, 4 ; number of iterations
+	mov rdi, 1 ; start rdi at 1
+  mov rsi, 4 ; number of iterations
   jmp masid;
 
 masid:
-  add ebx, ebx ; ebx += ebx
-	dec ecx ; exc -= 1
-  mov eax, 1 ; ecx -= 1
-  cmp ecx, 0 ; compare ecx with 0
+  add rdi, rdi ; rdi += rdi
+	dec rsi ; exc -= 1
+  cmp rsi, 0 ; compare rsi with 0
   jg masid ; jump to masid if greater
-  mov eax, 1 ; sys_exit system call
-  int 0x80
+  mov rax, 60 ; sys_exit system call
+  syscall
