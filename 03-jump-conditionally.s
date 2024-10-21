175002a960483bcc5d3ad3ac5f3c28da24a991f7
Guilhermo Masid <guilhermomasid@gmail.com>
2024-10-21 19:55:19 -0300

    transform jump-conditionally program into 64 bit

--- 03-jump-conditionally.s
+++ 03-jump-conditionally.s
@@ -2,12 +2,12 @@ global _start
 
 section .text
 _start:
-	mov ecx, 101 ; set ecx to 101
-  mov ebx, 42 ; exit status 42
-  mov eax, 1 ; sys_exit system call
-	cmp ecx, 100; compare ecx to 100
-  jl skip ; jump if less then 
-  mov ebx, 12 ; exit status is 12
+	mov rsi, 99 ; set rsi to 99
+  mov rdi, 42 ; exit status 42
+  mov rax, 60 ; sys_exit system call
+	cmp rsi, 100; compare rsi to 100
+  jl skip ; jump if less then (will jump)
+  mov rdi, 12 ; exit status is 12
 
 skip:
-  int 0x80
+  syscall
