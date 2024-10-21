18bdcaad7b509282169b43a56d1a525b054d6d6c
Guilhermo Masid <guilhermomasid@gmail.com>
2024-10-21 19:42:56 -0300

    transform jump program into 64 bit

--- 02-jump.s
+++ 02-jump.s
@@ -2,10 +2,10 @@ global _start
 
 section .text
 _start:
-  mov ebx, 42 ; exit status 42
-  mov eax, 1 ; sys_exit system call
+  mov rdi, 42 ; exit status 42
+  mov rax, 60 ; sys_exit system call
   jmp skip ; jump to "skip" label
-  mov ebx, 12 ; exit status is 12
+  mov rdi, 12 ; exit status is 12
 
 skip:
-  int 0x80
+  syscall
