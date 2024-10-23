1c192d47ac6fbf220a2e4c86b8595d63bde6bc90
Guilhermo Masid <guilhermomasid@gmail.com>
2024-10-23 19:29:11 -0300

    using ret instead of messing with registers

--- 07-functions.s
+++ 07-functions.s
@@ -1,11 +1,10 @@
 global _start
 
 _start:
-  call func ; this will push the next line into `rax` register
+  call func
 	mov rax, 60 ; sys_exit system call
   syscall
 
 func:
   mov rdi, 42 ; set status to be 42
-  pop rax ; now we pop the `rax`
-  jmp rax ; and jump to it
+  ret
