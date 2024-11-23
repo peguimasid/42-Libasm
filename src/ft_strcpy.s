4454a9994b1bd597117980809ea08853a62a3e34
Guilhermo Masid <guilhermomasid@gmail.com>
2024-11-23 17:24:26 -0300

    fix typo

--- src/ft_strcpy.s
+++ src/ft_strcpy.s
@@ -6,7 +6,7 @@ ft_strcpy:
 
 loop:
   mov al, [rsi + rcx] ; al = src[rcx]
-  mov [rdi + rcx], al ; dst[rcs] = al
+  mov [rdi + rcx], al ; dst[rcx] = al
   inc rcx             ; rcx++
   cmp al, 0           ; is al the end of string?
   jne loop            ; jump to loop if not is (jne -> jump if not equal)
