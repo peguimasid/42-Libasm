3018b95fb985e7f657dd04978af99e8adf585a12
Guilhermo Masid <guilhermomasid@gmail.com>
2024-11-25 19:53:29 -0300

    remove comments

--- src/ft_strcmp.s
+++ src/ft_strcmp.s
@@ -5,10 +5,10 @@ ft_strcmp:
   mov rcx, 0
 
 loop:
-  cmp byte [rdi + rcx], 0 ; is str[rcx] the end of the string?
-  je break                ; if is jump to break (je -> jump if equal)
-  inc rcx                 ; rcx++
-  jmp loop                ; repeat loop
+  cmp byte [rdi + rcx], 0
+  je break
+  inc rcx
+  jmp loop
 
 break:
   mov rax, rcx ; set the return value to be rcx
