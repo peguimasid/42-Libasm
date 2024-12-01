489ccb5138c270b4c186748af6932995f75822be
Guilhermo Masid <guilhermomasid@gmail.com>
2024-11-30 21:10:04 -0300

    add comments

--- src/ft_write.s
+++ src/ft_write.s
@@ -3,6 +3,10 @@ section .text
 
 ft_write:
   mov rax, 1
+  ; This is unnecessary because the registers already hold the values
+  ; mov rdi, rdi ; file descriptor
+  ; mov rsi, rsi ; buffer
+  ; mov rdx, rdx ; count
   syscall
   ret
 
