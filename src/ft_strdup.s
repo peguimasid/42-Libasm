0e8ad526567bafa43058744a1ddba439df136b0b
Guilhermo Masid <guilhermomasid@gmail.com>
2024-12-16 19:56:30 -0300

    add some comments

--- src/ft_strdup.s
+++ src/ft_strdup.s
@@ -7,14 +7,14 @@ extern ft_strcpy
 section .text
 
 ft_strdup:
-  call ft_strlen
-  inc rax
-  push rdi
-  mov rdi, rax
-  call malloc wrt ..plt
-  pop rsi
-  mov rdi, rax
-  call ft_strcpy
+  call ft_strlen        ; int result = strlen(string)
+  inc rax               ; result++
+  push rdi              ; tmp = string
+  mov rdi, rax          ; size = result
+  call malloc wrt ..plt ; result = malloc(size)
+  pop rsi               ; src = tmp
+  mov rdi, rax          ; dst = result
+  call ft_strcpy        ; result = ft_strcpy(dst, src)
   ret
 
 section .note.GNU-stack
