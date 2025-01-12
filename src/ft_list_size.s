54df2a40e9e6cec39b115718f8b6fd3fdd15a93f
Guilhermo Masid <guilhermomasid@gmail.com>
2025-01-11 22:17:16 -0300

    add comments to ft list size

--- src/ft_list_size.s
+++ src/ft_list_size.s
@@ -3,15 +3,15 @@ global ft_list_size
 section .text
 
 ft_list_size:
-  xor rax, rax
-  cmp rdi, 0
-  je break
+  xor rax, rax ; rax = 0
+  cmp rdi, 0   ; if list == NULL
+  je break     ; return
 
 loop:
-  inc rax
-  mov rdi, [rdi + 8]
-  cmp rdi, 0
-  jne loop
+  inc rax             ; result++
+  mov rdi, [rdi + 8]  ; list = list->next
+  cmp rdi, 0          ; if list != NULL
+  jne loop            ; jump to loop
 
 break:
   ret
